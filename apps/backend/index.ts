1fc5632bbaf29d11d17093ea6614501a4f223b59
Official-Krish <krishanand0103@gmail.com>
2025-05-28 02:23:40 +0530

    [route]: added route for jwt verification

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import userRouter from './routes/user';
 import meetingRouter from './routes/meeting';
+import { authMiddleware } from './utils/authMiddleware';
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,13 @@ app.use(cors());
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/meeting", meetingRouter);
+
+app.get("/api/v1/verify-token", authMiddleware,(req, res) => {
+    res.status(200).json({
+        message: "Token is valid",
+        user: req.userId,
+    });
+});
 
 app.listen(3000, () => {
     console.log('Backend server is running on port 3000');
