7cb616f045f3b30a9d2f29c7bdedc93c4b97b3ce
Official-Krish <krishanand0103@gmail.com>
2025-07-26 14:52:14 +0530

    [fix]: fixed footer and favicon

--- apps/client/src/components/Footer.tsx
+++ apps/client/src/components/Footer.tsx
@@ -1,6 +1,8 @@
 
 import { motion } from "framer-motion";
-import { X } from "lucide-react";
+import { FaGithub } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
@@ -27,22 +29,20 @@ const Footer = () => {
             <div className="flex space-x-4">
               {/* Social Media Icons */}
               <motion.a 
-                href="#" 
+                href="https://github.com/Official-Krish" 
                 className="h-10 w-10 flex items-center justify-center rounded-full border border-white/10 hover:bg-white/10 transition-colors"
                 whileHover={{ scale: 1.1, backgroundColor: "rgba(255,255,255,0.1)" }}
                 whileTap={{ scale: 0.9 }}
               >
-                <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-                  <path fillRule="evenodd" d="M22 12c0-5.523-4.477-10-10-10S2 6.477 2 12c0 4.991 3.657 9.128 8.438 9.878v-6.987h-2.54V12h2.54V9.797c0-2.506 1.492-3.89 3.777-3.89 1.094 0 2.238.195 2.238.195v2.46h-1.26c-1.243 0-1.63.771-1.63 1.562V12h2.773l-.443 2.89h-2.33v6.988C18.343 21.128 22 16.991 22 12z" clipRule="evenodd" />
-                </svg>
+                <FaGithub />
               </motion.a>
               <motion.a 
-                href="#" 
+                href="https://x.com/KrishAnand0103" 
                 className="h-10 w-10 flex items-center justify-center rounded-full border border-white/10 hover:bg-white/10 transition-colors"
                 whileHover={{ scale: 1.1, backgroundColor: "rgba(255,255,255,0.1)" }}
                 whileTap={{ scale: 0.9 }}
               >
-                <X className="h-5 w-5" />
+                <FaXTwitter />
               </motion.a>
               <motion.a 
                 href="#" 
